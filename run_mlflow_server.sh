f4716b80a41393da945c66a8230a12e84e6b858b
BOGDANCER1 <mle-user@epdc8qrd9ig7t1tscr9l.auto.internal>
2025-10-08 05:44:31 +0000

    Have practiced MLflow starting

---
+++ run_mlflow_server.sh
@@ -0,0 +1,4 @@
+mlflow server \
+  --backend-store-uri postgresql://mle_20250813_30977a78a3_freetrack:6c1e0f8b29874f1fa2e7f46c511dc49c@rc1b-uh7kdmcx67eomesf.mdb.yandexcloud.net:6432/playground_mle_20250813_30977a78a3\
+    --default-artifact-root s3://s3-student-mle-20250813-30977a78a3-freetrack \
+    --no-serve-artifacts
